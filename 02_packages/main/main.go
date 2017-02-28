1e8cba405a7772eef2f63b14d43737f51623adcf
DIKL <didikovski@web.de>
2017-02-28 10:56:39 +0100

    all

---
+++ 02_packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DIKL/Training/02_packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
